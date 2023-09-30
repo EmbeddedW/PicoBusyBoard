e86e2f92dc1dcb1cfc72400bb43c800416f57eee
Wojtek <wojstr@gmail.com>
2023-09-30 20:58:04 +0200

    Change pins into Pico board, play_melody need number of notes param

--- src/main.c
+++ src/main.c
@@ -354,12 +354,21 @@ void  play_melody(uint slice_num,note_struct * n,uint tempo)
 
 
 void blink_led();
-static const uint8_t PIN_PWM = 2u;
-
+static const uint8_t PIN_PWM = 14u;
+const uint LED_PIN = 25;
+uint  slice_num;
 
 int main() {
 
     stdio_init_all();    
+
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_set_function(PIN_PWM, GPIO_FUNC_PWM);
+    slice_num = pwm_gpio_to_slice_num(PIN_PWM); 
+
     xTaskCreate(blink_led, "Blink_led_task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
     return 0;
@@ -367,18 +376,12 @@ int main() {
 
 void blink_led(){
 
-    const uint LED_PIN = 0;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    gpio_set_function(PIN_PWM, GPIO_FUNC_PWM);
-    uint  slice_num = pwm_gpio_to_slice_num(PIN_PWM); 
 
     play_melody(slice_num, turnON, 200);
     vTaskDelay(500 / portTICK_PERIOD_MS);
 
     while (true) {
-        play_melody(slice_num, Mario_melody, 100);
+        //play_melody(slice_num, Mario_melody, 100);
         printf("Set ON LED\n");
         gpio_put(LED_PIN, 1);
         vTaskDelay(500 / portTICK_PERIOD_MS);
